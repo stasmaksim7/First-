8e53856bb8ec3b25f8031b330ace89146de58f31
stasmaksim7 <super-maksim07@mail.ru>
2025-09-16 00:22:39 +0300

    obnova

---
+++ First Project/src/Tasks_1_OAuP.cpp
@@ -0,0 +1,147 @@
+// Задачи 1 по ОАиП
+
+// Задание 1
+//#include <iostream>
+//using namespace std;
+//int main() {
+//	int a;
+//	cout << "Введите:";
+//	cin >> a;
+//	cout << "Кол-во метров:" << a / 100 << endl;
+//	cout << "Кол-во сантиметров:" << a % 100 << endl;
+//	return 0
+//}
+
+// Задание 2
+//#include <iostream>
+//using namespace std;
+//int main() {
+//	int a,b;
+//	cout << "Введите длину:" << endl;
+//	cin >> a;
+//	cout << "Введите ширину:" << endl;
+//	cin >> b;
+//	cout << "Площадь прямоугольника:" << a * b << endl;
+//	return 0;
+//}
+//
+
+// Задание 3
+//#include <iostream>
+//using namespace std;
+//int main() {
+//	float a = 0;
+//	cout << "Введите температу в градусах цельсия:"<<endl;
+//	cin >> a;
+//	float b = (a * 1.8 ) + 32;
+//	cout << "Температура по фарингейту:" << b << endl;
+//	return 0;
+//}
+
+// Задание 4
+//#include <iostream>
+//using namespace std;
+//int main() {
+//	int a;
+//	cout << "Введите число:" << endl;
+//	cin >> a;
+//	if (a % 2 == 0){
+//		cout << "Число четное" << endl;
+//	}
+//	else {
+//		cout << "Число нечетное" << endl;
+//	}
+//	return 0;
+//}
+
+// Задание 5
+//#include <iostream>
+//using namespace std;
+//int main() {
+//	int a;
+//	cout << "Введите число:" << endl;
+//	cin >> a;
+//	if ((a % 3 == 0) && (a % 5 == 0)) {
+//		cout << "Число делится и на 5, и на 3" << endl;
+//	}
+//	else if (a % 3 == 0) {
+//		cout << "Число делится только на 3" << endl;
+//	}
+//	else if (a % 5 == 0) {
+//		cout << "Число делится только на 5" << endl;
+//	}
+//	else {
+//		cout << "Число не делится ни на 5, ни на 3" << endl;
+//	}
+//	return 0;
+//}
+
+// Задание 6
+//#include <iostream>
+//using namespace std;
+//int main() {
+//	int a;
+//	cout << "Введите свой возраст:" << endl;
+//	cin >> a;
+//	if (a < 18) {
+//		cout << "Ваш возраст меньше 18" << endl;
+//	}
+//	else if (a <= 60) {
+//		cout << "Ваш возраст от 18 до 60 включительно" << endl;
+//	}
+//	else {
+//		cout << "Ваш возраст больше 60" << endl;
+//	}
+//	return 0;
+//}
+
+// Задание 7
+//#include <iostream>
+//using namespace std;
+//int main() {
+//	int a;
+//	cout << "Введите число:" << endl;
+//	cin >> a;
+//	for (int i = 1;i <= a;i++) {
+//		cout << i*i << endl;
+//	}
+//	return 0;
+//}
+
+// Задание 8 - НЕДОДЕЛАНО
+//#include <iostream>
+//using namespace std;
+//int main() {
+//	int k = 0;
+//	int flag = 1;
+//	while (flag == 1) {
+//		int a;
+//		cout << "Введите число:" << endl;
+//		cin >> a;
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
