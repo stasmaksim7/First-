79652865509977d69205d4a596e916a8f18cd22a
stasmaksim7 <super-maksim07@mail.ru>
2025-09-19 08:58:06 +0300

    1_1

---
+++ First Project/src/Laboratory_work1_Task2_Infa.cpp
@@ -0,0 +1,19 @@
+// Лабораторная работа номер 1 по информатике Вариант 1
+
+// Задание 2
+
+//#include <iostream>
+//using namespace std;
+//int main() {
+//	double k = 10;
+//	for (int i = 1; i <= 10;i++){
+//		cout << i << "  " << k << endl;
+//		k += 10;
+//	}
+//	return 0;
+//}
+
+
+
+
+
